346a94257443a8fde10a832043629f9c367918f9
ttossavainen <teemu.tossavainen@hiq.fi>
2023-03-20 14:41:10 +0200

    init

--- Frends.ODBC.ExecuteQuery/Frends.ODBC.ExecuteQuery.Tests/UnitTests.cs
+++ Frends.ODBC.ExecuteQuery/Frends.ODBC.ExecuteQuery.Tests/UnitTests.cs
@@ -8,6 +8,8 @@ public class UnitTests
 {
     // Create OdbcDsn using PowerShell as admin (check file location):
     //Add-OdbcDsn -Name "ODBC_testDB" -DriverName "Microsoft Access Driver (*.mdb, *.accdb)" -DsnType "User" -Platform "64-bit" -SetPropertyValue "Dbq=$pwd\TestFiles\ODBC_testDB.accdb"
+    // or
+    //Add-OdbcDsn -Name "ODBC_testDB" -DriverName "Microsoft Access Driver (*.mdb, *.accdb)" -DsnType "User" -Platform "32-bit" -SetPropertyValue "Dbq=$pwd\TestFiles\ODBC_testDB_32.mdb"
 
     // Remove
     //Remove-OdbcDsn -Name "ODBC_testDB" -DsnType "User" -Platform "64-bit"
