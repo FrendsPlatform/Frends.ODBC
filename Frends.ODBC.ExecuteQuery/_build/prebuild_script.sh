baf5029f0e39c3f82ffc9fc7aeb03228cf123f81
Riku Virtanen <virtanen.riku91@gmail.com>
2023-05-09 09:49:41 +0300

    Testing CI

--- Frends.ODBC.ExecuteQuery/_build/prebuild_script.sh
+++ Frends.ODBC.ExecuteQuery/_build/prebuild_script.sh
@@ -1,5 +1,4 @@
-﻿#!/usr/bin/env
-
+﻿#!/bin/bash
 mkdir -p  /etc/odbc
 apt-get update
 apt-get -y install sudo
@@ -11,18 +10,14 @@ apt-get -y install odbcinst
 apt update
 apt -y install dos2unix
 apt -y install lsb-release
-
 if ! [[ "18.04 20.04 22.04" == *"$(lsb_release -rs)"* ]];
 then
     echo "Ubuntu $(lsb_release -rs) is not currently supported.";
     exit;
 fi
-
 sudo su
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-
 curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list
-
 sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
 # optional: for bcp and sqlcmd
 sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
@@ -30,6 +25,5 @@ echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
 source ~/.bashrc
 # optional: for unixODBC development headers
 sudo apt-get install -y unixodbc-dev
-
-cp ./odbcinst.init ../../../../../../etc/odbcinst.init
+sudo cp ./odbcinst.init ../../../../../../etc/odbcinst.init
 
