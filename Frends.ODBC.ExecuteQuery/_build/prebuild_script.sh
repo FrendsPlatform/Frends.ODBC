c3bf3d2a221862d985872b515fc39b10cf5805eb
Riku Virtanen <virtanen.riku91@gmail.com>
2023-05-09 14:24:21 +0300

    More debugging

--- Frends.ODBC.ExecuteQuery/_build/prebuild_script.sh
+++ Frends.ODBC.ExecuteQuery/_build/prebuild_script.sh
@@ -7,8 +7,9 @@ sudo apt-get -y install gnupg
 sudo apt-get -y install mdbtools
 sudo apt-get -y install odbcinst
 sudo apt-get -y install lsb-release
-sudo su -c curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-sudo su -c curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > ./mssql-release.list
+sudo cp ./mssql-release.list ../../../../../etc/apt/sources.list.d/mssql-release.list
 sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
 # optional: for bcp and sqlcmd
 sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
@@ -16,5 +17,5 @@ echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
 source ~/.bashrc
 # optional: for unixODBC development headers
 sudo apt-get install -y unixodbc-dev
-sudo cp ./_build/odbcinst.init ../../../../../etc/odbcinst.init
-echo "file odbcinst.init copied to etc/odbcinst.init"
+sudo cp ./_build/odbcinst.ini ../../../../../etc/odbcinst.ini
+echo "file odbcinst.ini copied to /etc/odbcinst.ini"
