a5c80f56065248e4f2519d8ff7fdc71f6e53cf57
Riku Virtanen <virtanen.riku91@gmail.com>
2023-05-09 14:28:32 +0300

    More debugging

--- Frends.ODBC.ExecuteQuery/_build/prebuild_script.sh
+++ Frends.ODBC.ExecuteQuery/_build/prebuild_script.sh
@@ -9,7 +9,7 @@ sudo apt-get -y install odbcinst
 sudo apt-get -y install lsb-release
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > ./mssql-release.list
-sudo cp ./mssql-release.list ../../../../../etc/apt/sources.list.d/mssql-release.list
+sudo cp -f ./mssql-release.list /etc/apt/sources.list.d/
 sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
 # optional: for bcp and sqlcmd
 sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
@@ -17,5 +17,4 @@ echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
 source ~/.bashrc
 # optional: for unixODBC development headers
 sudo apt-get install -y unixodbc-dev
-sudo cp ./_build/odbcinst.ini ../../../../../etc/odbcinst.ini
-echo "file odbcinst.ini copied to /etc/odbcinst.ini"
+sudo cp -f ./_build/odbcinst.ini /etc/
