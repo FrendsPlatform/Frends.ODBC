399ef74d48deca748388656958e06ee4b64f959b
Riku Virtanen <virtanen.riku91@gmail.com>
2023-05-16 09:52:47 +0300

    Testing CI

--- Frends.ODBC.ExecuteQuery/_build/prebuild_script.ps1
+++ Frends.ODBC.ExecuteQuery/_build/prebuild_script.ps1
@@ -1,5 +1,21 @@
-﻿Set-ExecutionPolicy RemoteSigned
+﻿$osName = $PSVersionTable.OS
+Write-Host "The current operating system is: $osName"
 
-Get-OdbcDriver -Name "SQL Server*" -Platform "64-bit"
-
-Add-OdbcDsn -Name "ODBC_testDB" -DriverName "SQL Server" -DsnType "User" -Platform "64-bit" -SetPropertyValue @("Name=ODBC_testDB", "Server=localhost", "Trusted_Connection=Yes", "Database=UnitTests")
+if ([Environment]::Is64BitOperatingSystem) {
+	Write-Host "Get file for $osName"
+    	Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=2223304" -OutFile "$PWD/odbc.msi"
+	Write-Host "File for $osName downloaded"
+	Start-Process -FilePath "$PWD/odbc.msi" -ArgumentList "/qr IACCEPTMSODBCSQLLICENSETERMS=YES"
+	Write-Host "Installation complete"
+	Add-OdbcDsn -Name "ODBC_testDB" -DriverName "ODBC Driver 17 for SQL Server" -DsnType "User" -Platform "64-bit" -SetPropertyValue @("Name=ODBC_testDB", "Server=localhost", "Trusted_Connection=Yes", "Database=UnitTests")
+	Write-Host "ODBC_testDB.accdb added"
+}
+else {
+	Write-Host "Get file for $osName"
+    	Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=2223303" -OutFile "$PWD/odbc.msi"
+	Write-Host "File for $osName downloaded"
+	Start-Process -FilePath "$PWD/odbc.msi" -ArgumentList "/qr IACCEPTMSODBCSQLLICENSETERMS=YES"
+	Write-Host "Installation complete"
+	Add-OdbcDsn -Name "ODBC_testDB" -DriverName "ODBC Driver 17 for SQL Server" -DsnType "User" -Platform "32-bit" -SetPropertyValue @("Name=ODBC_testDB", "Server=localhost", "Trusted_Connection=Yes", "Database=UnitTests")
+	Write-Host "ODBC_testDB_32.mdb added"
+}
