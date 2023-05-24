ee8e2ba2475803603c40e502bf91ade7ee2f130c
ttossavainen <teemu.tossavainen@hiq.fi>
2023-05-24 08:28:05 +0300

    DB files

---
+++ Frends.ODBC.ExecuteQuery/Frends.ODBC.ExecuteQuery.Tests/DB/init-db.sql
@@ -0,0 +1,13 @@
+﻿ALTER LOGIN sa ENABLE;
+GO
+ALTER LOGIN sa WITH PASSWORD = 'yourStrong!Password';
+GO
+CREATE DATABASE UnitTests;
+GO
+USE UnitTests;
+GO
+CREATE TABLE AnimalTypes (Id INT, Animal nvarchar(255), PRIMARY KEY(Id));
+GO
+INSERT INTO AnimalTypes (Id, Animal)
+VALUES (1, 'Mammal'), (2, 'Bird');
+GO
