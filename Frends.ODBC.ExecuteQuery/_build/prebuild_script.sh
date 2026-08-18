828dc3952fed17a4fa3fb8cdc94268fc43c9d7d7
Mateusz Noga-Wojtania <44415151+MatteoDelOmbra@users.noreply.github.com>
2026-08-18 12:39:12 +0200

    Update prebuild script for Microsoft ODBC installation

--- Frends.ODBC.ExecuteQuery/_build/prebuild_script.sh
+++ Frends.ODBC.ExecuteQuery/_build/prebuild_script.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -e
-apt-get update
-apt-get install -y curl gnupg mdbtools odbcinst lsb-release unixodbc-dev
 
-curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
-curl -fsSL https://packages.microsoft.com/config/ubuntu/22.04/prod.list | tee /etc/apt/sources.list.d/mssql-release.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y curl gnupg mdbtools odbcinst lsb-release unixodbc-dev
 
-apt-get update
-ACCEPT_EULA=Y apt-get install -y msodbcsql17 mssql-tools
+sudo rm -f /etc/apt/sources.list.d/mssql-release.list
+sudo rm -f /etc/apt/sources.list.d/microsoft-prod.list
+
+curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg --overwrite
+curl -fsSL https://packages.microsoft.com/config/ubuntu/22.04/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list > /dev/null
+
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql17 mssql-tools
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
