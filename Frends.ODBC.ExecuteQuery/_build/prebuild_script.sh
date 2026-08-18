77e0deb95e67e762cdd9c52aaffd47dce7b47cea
Mateusz Noga-Wojtania <44415151+MatteoDelOmbra@users.noreply.github.com>
2026-08-18 12:46:40 +0200

    Update GPG command to use batch mode

--- Frends.ODBC.ExecuteQuery/_build/prebuild_script.sh
+++ Frends.ODBC.ExecuteQuery/_build/prebuild_script.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y curl gnupg mdbtools odbcinst lsb-release unixodbc-dev
 sudo rm -f /etc/apt/sources.list.d/mssql-release.list
 sudo rm -f /etc/apt/sources.list.d/microsoft-prod.list
 
-curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg -y
+curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
 curl -fsSL https://packages.microsoft.com/config/ubuntu/22.04/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list > /dev/null
 
 sudo apt-get update
