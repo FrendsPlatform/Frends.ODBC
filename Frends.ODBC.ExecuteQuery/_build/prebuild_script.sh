ac7085fa48a10aeb0812d7b0d991eab720b34e1e
Mateusz Noga-Wojtania <44415151+MatteoDelOmbra@users.noreply.github.com>
2026-08-18 12:34:54 +0200

    Refactor prebuild_script.sh for cleaner installation
    Simplified installation script by removing unnecessary sudo commands and combining package installations.

--- Frends.ODBC.ExecuteQuery/_build/prebuild_script.sh
+++ Frends.ODBC.ExecuteQuery/_build/prebuild_script.sh
@@ -1,24 +1,11 @@
-﻿#!/bin/bash
-echo $PWD
-sudo su
-sudo apt-get update
-sudo apt-get -y install curl
-sudo apt-get -y install gnupg
-sudo apt-get -y install mdbtools
-sudo apt-get -y install odbcinst
-sudo apt-get -y install lsb-release
+#!/bin/bash
+set -e
+apt-get update
+apt-get install -y curl gnupg mdbtools odbcinst lsb-release unixodbc-dev
 
-curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc > /dev/null
+curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
+curl -fsSL https://packages.microsoft.com/config/ubuntu/22.04/prod.list | tee /etc/apt/sources.list.d/mssql-release.list > /dev/null
 
-curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list > ./mssql-release.list
-
-sudo mv ./mssql-release.list /etc/apt/sources.list.d/mssql-release.list
-sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install -y --allow-unauthenticated msodbcsql17
-# optional: for bcp and sqlcmd
-sudo ACCEPT_EULA=Y apt-get install -y --allow-unauthenticated mssql-tools
-echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql17 mssql-tools
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
-
-# optional: for unixODBC development headers
-sudo apt-get install -y unixodbc-dev
